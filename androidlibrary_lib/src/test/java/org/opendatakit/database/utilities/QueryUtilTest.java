1d1451f56b1652e0a58492592c90100c7cd12ee1
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-10-26 20:01:19 +0100

    add tests with where and having clause

--- androidlibrary_lib/src/test/java/org/opendatakit/database/utilities/QueryUtilTest.java
+++ androidlibrary_lib/src/test/java/org/opendatakit/database/utilities/QueryUtilTest.java
@@ -12,4 +12,19 @@ public class QueryUtilTest {
         String sql = QueryUtil.buildSqlStatement(tableId, null, null, null, null, null);
         assertEquals("SELECT * FROM \"test_table\" ", sql);
     }
+
+    @Test
+    public void buildSqlStatement_withWhereClause_returnWhereStatement() {
+        String whereClause = "name = 'Bob'";
+        String sql = QueryUtil.buildSqlStatement("test_table", whereClause, null, null, null, null);
+        assertEquals("SELECT * FROM \"test_table\" WHERE name = 'Bob'", sql);
+    }
+
+    @Test
+    public void buildSqlStatement_withHavingClause_returnHavingStatement() {
+        String[] groupBy = { "class" };
+        String havingClause = "COUNT(class) > 4";
+        String sql = QueryUtil.buildSqlStatement("test_table", null, groupBy, havingClause, null, null);
+        assertEquals("SELECT * FROM \"test_table\" GROUP BY class HAVING COUNT(class) > 4", sql);
+    }
 }
