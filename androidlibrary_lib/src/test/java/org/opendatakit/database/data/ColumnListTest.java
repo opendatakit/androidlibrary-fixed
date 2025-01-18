c486a7e2672e046cb0d01459cb1f77b16597d45e
Bukunmi Ola <ebukunmiola@gmail.com>
2025-01-18 08:02:33 +0100

    PR 292 fix

--- androidlibrary_lib/src/test/java/org/opendatakit/database/data/ColumnListTest.java
+++ androidlibrary_lib/src/test/java/org/opendatakit/database/data/ColumnListTest.java
@@ -1,10 +1,8 @@
 package org.opendatakit.database.data;
 
-import android.os.Parcel;
 import org.junit.Before;
 import org.junit.Test;
 import org.opendatakit.aggregate.odktables.rest.entity.Column;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,14 +10,21 @@ import static org.junit.Assert.*;
 
 public class ColumnListTest {
 
+    private static final String ELEMENT_KEY_1 = "elementKey1";
+    private static final String ELEMENT_NAME_1 = "elementName1";
+    private static final String ELEMENT_KEY_2 = "elementKey2";
+    private static final String ELEMENT_NAME_2 = "elementName2";
+    private static final String ELEMENT_TYPE_STRING = "string";
+    private static final String CHILD_KEY_1 = "childKeys1";
+    private static final String CHILD_KEY_2 = "childKeys2";
     private List<Column> testColumns;
 
     @Before
     public void setUp() {
         // Initialize a list of columns
         testColumns = new ArrayList<>();
-        testColumns.add(new Column("elementKey1", "elementName1", "elementType1", "childKeys1"));
-        testColumns.add(new Column("elementKey2", "elementName2", "elementType2", "childKeys2"));
+        testColumns.add(new Column(ELEMENT_KEY_1, ELEMENT_NAME_1, ELEMENT_TYPE_STRING, CHILD_KEY_1));
+        testColumns.add(new Column(ELEMENT_KEY_2, ELEMENT_NAME_2, ELEMENT_TYPE_STRING, CHILD_KEY_2));
     }
 
     @Test
@@ -27,64 +32,5 @@ public class ColumnListTest {
         ColumnList columnList = new ColumnList(testColumns);
         assertNotNull(columnList);
         assertEquals(testColumns, columnList.getColumns());
-    }
-
-
-    @Test
-    public void WriteToParcel() {
-        ColumnList columnList = new ColumnList(testColumns);
-        Parcel parcel = Parcel.obtain();
-        columnList.writeToParcel(parcel, 0);
-
-        parcel.setDataPosition(0);
-
-        ColumnList createdFromParcel = ColumnList.CREATOR.createFromParcel(parcel);
-        assertEquals(testColumns.size(), createdFromParcel.getColumns().size());
-
-        for (int i = 0; i < testColumns.size(); i++) {
-            Column original = testColumns.get(i);
-            Column fromParcel = createdFromParcel.getColumns().get(i);
-            assertEquals(original.getElementKey(), fromParcel.getElementKey());
-            assertEquals(original.getElementName(), fromParcel.getElementName());
-            assertEquals(original.getElementType(), fromParcel.getElementType());
-            assertEquals(original.getListChildElementKeys(), fromParcel.getListChildElementKeys());
-        }
-
-        parcel.recycle();
-    }
-
-    @Test
-    public void ReadFromParcel() {
-        Parcel parcel = Parcel.obtain();
-        parcel.writeInt(2);
-
-        for (Column column : testColumns) {
-            parcel.writeString(column.getElementKey());
-            parcel.writeString(column.getElementName());
-            parcel.writeString(column.getElementType());
-            parcel.writeString(column.getListChildElementKeys());
-        }
-
-        parcel.setDataPosition(0);
-
-        ColumnList columnList = ColumnList.CREATOR.createFromParcel(parcel);
-        assertEquals(testColumns.size(), columnList.getColumns().size());
-
-        for (int i = 0; i < testColumns.size(); i++) {
-            Column original = testColumns.get(i);
-            Column fromParcel = columnList.getColumns().get(i);
-            assertEquals(original.getElementKey(), fromParcel.getElementKey());
-            assertEquals(original.getElementName(), fromParcel.getElementName());
-            assertEquals(original.getElementType(), fromParcel.getElementType());
-            assertEquals(original.getListChildElementKeys(), fromParcel.getListChildElementKeys());
-        }
-
-        parcel.recycle();
-    }
-
-    @Test
-    public void ParcelCreator() {
-        ColumnList[] columnArray = ColumnList.CREATOR.newArray(2);
-        assertEquals(2, columnArray.length);
     }
 }
