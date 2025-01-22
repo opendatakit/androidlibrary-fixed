bf79e5e31325274bbf4749df0a9ceb00de4f5820
Bukunmi Ola <ebukunmiola@gmail.com>
2025-01-22 14:01:15 +0100

    cleanup

--- androidlibrary_lib/src/androidTest/java/org/opendatakit/logging/WebLoggerFactoryImplTest.java
+++ androidlibrary_lib/src/androidTest/java/org/opendatakit/logging/WebLoggerFactoryImplTest.java
@@ -4,13 +4,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.opendatakit.utilities.ODKFileUtils;
+import static org.junit.Assert.assertTrue;
+
 
 import java.io.File;
 
 public class WebLoggerFactoryImplTest {
 
     private WebLoggerFactoryImpl loggerFactory;
-    private String testAppName = "testApp";
+    private final String testAppName = "testApp";
 
     @Before
     public void setUp() {
@@ -20,34 +22,24 @@ public class WebLoggerFactoryImplTest {
     @After
     public void tearDown() {
         File loggingFolder = new File(ODKFileUtils.getLoggingFolder(testAppName));
-        deleteDirectory(loggingFolder);
+        loggingFolder.delete();
     }
 
     @Test
     public void WithValidAppName_ShouldReturnWebLoggerImpl() {
         WebLoggerIf logger = loggerFactory.createWebLogger(testAppName);
 
-        org.junit.Assert.assertTrue("Expected WebLoggerImpl instance", logger instanceof WebLoggerImpl);
+        assertTrue("Expected WebLoggerImpl instance", logger instanceof WebLoggerImpl);
 
         File loggingFolder = new File(ODKFileUtils.getLoggingFolder(testAppName));
-        org.junit.Assert.assertTrue("Expected logging folder to exist", loggingFolder.exists());
-        org.junit.Assert.assertTrue("Expected logging folder to be a directory", loggingFolder.isDirectory());
+        assertTrue("Expected logging folder to exist", loggingFolder.exists());
+        assertTrue("Expected logging folder to be a directory", loggingFolder.isDirectory());
     }
 
     @Test
     public void WithNullAppName_ShouldReturnWebLoggerAppNameUnknownImpl() {
         WebLoggerIf logger = loggerFactory.createWebLogger(null);
 
-        org.junit.Assert.assertTrue("Expected WebLoggerAppNameUnknownImpl instance", logger instanceof WebLoggerAppNameUnknownImpl);
-    }
-
-    private void deleteDirectory(File directoryToBeDeleted) {
-        File[] allContents = directoryToBeDeleted.listFiles();
-        if (allContents != null) {
-            for (File file : allContents) {
-                deleteDirectory(file);
-            }
-        }
-        directoryToBeDeleted.delete();
+        assertTrue("Expected WebLoggerAppNameUnknownImpl instance", logger instanceof WebLoggerAppNameUnknownImpl);
     }
 }
